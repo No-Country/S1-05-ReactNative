dd0fa7f6687f8471d9e5b272bcd93c60b4f5a6d8
Bryan Scalzo <50776347+bryanscalzo97@users.noreply.github.com>
2022-04-07 14:22:04 -0300

    add sellCrypto screen

--- src/screens/BuyCrypto/BuyCrypto.js
+++ src/screens/BuyCrypto/BuyCrypto.js
@@ -1,21 +1,36 @@
-import { View, Text, Button, TextInput } from "react-native";
+import { View, Text, Button, TextInput, Picker, Alert } from "react-native";
 import React, { useState } from "react";
 import styles from "./styles";
+import { MaterialCommunityIcons } from "@expo/vector-icons/";
 
 const BuyCrypto = () => {
   const [crypto, setCrypto] = useState("0");
+  const [selectedValue, setSelectedValue] = useState("BTC");
   return (
     <View style={styles.addCashContainer}>
       <View style={styles.textContainer}>
         <View style={styles.cryptoCalculator}>
           <View style={styles.cryptoMenu}>
-            <Text style={styles.currency}>pesos</Text>
+            <Text>DOLAR</Text>
           </View>
-          <View>
-            <Text style={styles.arrow}>--x</Text>
+          <View style={styles.arrow}>
+            <MaterialCommunityIcons
+              color="black"
+              name="arrow-right"
+              size={30}
+            />
           </View>
-          <View>
-            <Text style={styles.cryptoCurrency}>hola</Text>
+          <View style={styles.cryptoCurrency}>
+            <Picker
+              selectedValue={selectedValue}
+              style={{ height: 50, width: 100 }}
+              onValueChange={(itemValue, itemIndex) =>
+                setSelectedValue(itemValue)
+              }
+            >
+              <Picker.Item label="BTC" value="BTC" />
+              <Picker.Item label="ETH" value="ETH" />
+            </Picker>
           </View>
         </View>
         <View style={styles.inputContainer}>
@@ -28,6 +43,9 @@ const BuyCrypto = () => {
             placeholder="0"
           />
         </View>
+        <View style={styles.cryptoChange}>
+          <Text style={styles.textChange}>= 0.00000 {selectedValue}</Text>
+        </View>
       </View>
       <View style={styles.buttonContainer}>
         <Button
@@ -35,7 +53,7 @@ const BuyCrypto = () => {
           title="BUY"
           color="#4D6CFE"
           accessibilityLabel="ADD MONEY"
-          onPress={() => Alert.alert("money added successfully")}
+          onPress={() => Alert.alert("Crypto bought successfully")}
         />
       </View>
     </View>
--- src/screens/SellCrypto/SellCrypto.js
+++ src/screens/SellCrypto/SellCrypto.js
@@ -1,11 +1,61 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Button, TextInput, Picker, Alert } from "react-native";
+import React, { useState } from "react";
 import styles from "./styles";
+import { MaterialCommunityIcons } from "@expo/vector-icons/";
 
 const SellCrypto = () => {
+  const [crypto, setCrypto] = useState("0");
+  const [selectedValue, setSelectedValue] = useState("BTC");
   return (
-    <View>
-      <Text>SellCrypto</Text>
+    <View style={styles.addCashContainer}>
+      <View style={styles.textContainer}>
+        <View style={styles.cryptoCalculator}>
+          <View style={styles.cryptoCurrency}>
+            <Picker
+              selectedValue={selectedValue}
+              style={{ height: 50, width: 100 }}
+              onValueChange={(itemValue, itemIndex) =>
+                setSelectedValue(itemValue)
+              }
+            >
+              <Picker.Item label="BTC" value="BTC" />
+              <Picker.Item label="ETH" value="ETH" />
+            </Picker>
+          </View>
+          <View style={styles.arrow}>
+            <MaterialCommunityIcons
+              color="black"
+              name="arrow-right"
+              size={30}
+            />
+          </View>
+          <View style={styles.cryptoMenu}>
+            <Text>DOLAR</Text>
+          </View>
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.currency}>$</Text>
+          <TextInput
+            autoFocus={true}
+            style={styles.input}
+            onChangeText={setCrypto}
+            keyboardType="numeric"
+            placeholder="0"
+          />
+        </View>
+        <View style={styles.cryptoChange}>
+          <Text style={styles.textChange}>= 0.00000 {selectedValue}</Text>
+        </View>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          style={styles.button}
+          title="SELL"
+          color="#4D6CFE"
+          accessibilityLabel="SELL CRYPTO"
+          onPress={() => Alert.alert("Crypto sold successfully")}
+        />
+      </View>
     </View>
   );
 };
